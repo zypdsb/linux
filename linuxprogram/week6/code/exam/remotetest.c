6902edefaf00310c7231a1b28079dadac5faefa7
zypdsb <1327060448@qq.com>
2019-10-15 03:28:18 -0700

    linux-homework

---
+++ linuxprogram/week6/code/exam/remotetest.c
@@ -0,0 +1,9 @@
+#include "staticlib.h"
+#include<stdio.h>
+int main(){
+  int a[5];
+  initarray(a,5);
+  showarray(a,5);
+  printf("max:%d\n",max(a,5));
+  printf("max:%d\n",sum(a,5));
+}
---
+++ linuxprogram/week6/code/staticlib/staticlib.h
@@ -0,0 +1,6 @@
+//array operations
+int max(int *,int);
+int sum(int *,int);
+//initial array
+void initarray(int *,int);
+void showarray(int *,int);
