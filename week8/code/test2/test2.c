70eb80e04ecc74c6439df8f0d1c0219862ece0f3
zypdsb <1327060448@qq.com>
2019-11-01 20:50:31 -0700

    week8 two

---
+++ week8/code/test2/test2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int a;
+static int b=10;
+int main(int argc,char* argv[],char* envp[]){
+    int a=10;
+    char* p=(char*)malloc(sizeof(char));
+    printf("a=%d,p=0x%x\n",a,p);
+    getchar();
+    getchar();
+    return 0;
+}
+
